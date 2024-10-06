18cafe52d335e6253dde9e692b3c3415b68d7afc
vieter97 <vieter97@gmail.com>
2024-10-06 17:55:13 +0000

    Added center to pages and shrekfedora to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to Viet Tran&apos;s Portfolio</h1>
-      <Image src="/viet.png" alt="Viet Tran" width={250} height={250} />
+      <Image src="/shrekfedora.jpg" alt="Viet Tran" width={250} height={250} />
       <p className="text-center max-w-2xl">
         I&apos;m an Integration Developer at Frends Enterprise iPaaS with a background in Software Engineering. 
         This website showcases my skills, projects, and professional experience.
